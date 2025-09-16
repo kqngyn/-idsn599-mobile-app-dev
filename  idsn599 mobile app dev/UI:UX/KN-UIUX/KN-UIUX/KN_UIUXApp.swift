3d7db367886607265b1acd3f0a465aeb895cdecb
kqngyn <khn037@ucsd.edu>
2025-09-16 01:39:33 -0700

    finished draft of exercise 3

---
+++  idsn599 mobile app dev/UI:UX/KN-UIUX/KN-UIUX/KN_UIUXApp.swift
@@ -0,0 +1,17 @@
+//
+//  KN_UIUXApp.swift
+//  KN-UIUX
+//
+//  Created by Khoi Nguyen on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KN_UIUXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
